f0762c0f6568f46a41a812aa59f2be30888c4dce
fnoir <35193356+fnoir@users.noreply.github.com>
2019-08-07 15:28:55 +0200

    Delete index.php

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Cornfield\Core\Bootstrap;
-
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
-
-new Bootstrap();
