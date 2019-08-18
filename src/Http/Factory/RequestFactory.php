71771f209ef469d10284227e32b115df7c3c8929
fnoir <35193356+fnoir@users.noreply.github.com>
2019-08-18 18:31:27 +0200

    Add RequestMethodInterface

--- src/Http/Factory/RequestFactory.php
+++ src/Http/Factory/RequestFactory.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Cornfield\Core\Http\Factory;
 
+use Cornfield\Core\Router\RequestMethodInterface;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 
-final class RequestFactory implements RequestFactoryInterface
+final class RequestFactory implements RequestFactoryInterface, RequestMethodInterface
 {
     /**
      * {@inheritdoc}
