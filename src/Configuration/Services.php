be2fcdfdc4a7da23bcdac528348a159c22d92661
fnoir <35193356+fnoir@users.noreply.github.com>
2019-12-05 21:10:13 +0100

    Update Services.php

--- src/Configuration/Services.php
+++ src/Configuration/Services.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Cornfield\Core\Configuration;
 
 use Cornfield\Core\Exception\InvalidParameterException;
+use Cornfield\Core\I18n\I18nInterface;
+use Cornfield\Core\I18n\JsonI18n;
 use Cornfield\Core\Session\NativeSession;
 use Cornfield\Core\Session\SessionInterface;
 use Cornfield\Core\Template\TemplateInterface;
@@ -24,6 +26,17 @@ return [
         }
 
         return new Psr16Cache(new NullAdapter());
+    },
+    I18nInterface::class => static function (ContainerInterface $container): I18nInterface {
+        if (false === $container->has('i18n.path.language.default')) {
+            throw new InvalidParameterException('The key "i18n.path.language.default" is undefined');
+        }
+
+        return new JsonI18n(
+            $container->get(CacheInterface::class),
+            (string) $container->get('i18n.path.language.default'),
+            $container->has('i18n.path.language.callback') ? (string) $container->has('i18n.path.language.callback') : ''
+        );
     },
     SessionInterface::class => static function (ContainerInterface $container): SessionInterface {
         $options = $container->has('session.options') ? $container->get('session.options') : [];
